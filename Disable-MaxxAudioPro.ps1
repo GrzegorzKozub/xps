d231caeede44522f933dcc679c6cf37966e85d2f
Grzegorz Kozub <grzegorz.kozub@gmail.com>
2017-10-24 10:16:34 +0200

    Disable another startup item they recently introduced

--- Disable-MaxxAudioPro.ps1
+++ Disable-MaxxAudioPro.ps1
@@ -13,6 +13,7 @@ $regFileContent = @"
 Windows Registry Editor Version 5.00
 
 [HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\StartupApproved\Run]
+"RtHDVBg_PushButton"=hex:03,00,00,00,0b,34,17,77,53,4c,d3,01
 "RtHDVBg_WAVES_SKYLAKE"=hex(3):03,00,00,00,4F,89,38,1E,5D,A1,D2,01
 "RTHDVCPL"=hex(3):03,00,00,00,10,C0,D1,1F,5D,A1,D2,01
 "WavesSvc"=hex(3):03,00,00,00,F8,39,39,21,5D,A1,D2,01
